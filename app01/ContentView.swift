c385b7a0e9cd2284b5ccc3c4d7948febb480b549
Furkan Yanteri <furkanyanteri@gmail.com>
2025-05-04 16:28:06 +0300

    chore: git integration test commit

--- app01/ContentView.swift
+++ app01/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, frkn!")
+            Text("Hello, frkn2!")
         }
         .padding()
     }
