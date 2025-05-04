33e02a4fd8f1cde3f2f4d45f862216830824dcde
Furkan Yanteri <furkanyanteri@gmail.com>
2025-05-04 16:11:37 +0300

    Initial Commit

---
+++ app01/app01App.swift
@@ -0,0 +1,17 @@
+//
+//  app01App.swift
+//  app01
+//
+//  Created by Furkan Yanteri on 4.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct app01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
